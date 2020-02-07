b4cc066d5e606d4dd0fffe4eab0d88dcb280dfa4
Stephen Uweh <38926961+stephen-uweh@users.noreply.github.com>
2020-02-07 19:11:30 +0100

    Add files via upload

---
+++ groupchat/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '3eb1cfb7d8138819aabbaedf1c9198bc'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
+from groupchat import routes
